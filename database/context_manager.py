10aba7c640e0718c7c93728c3e4092f934c20356
johnconnor77 <jfcalleh@unal.edu.co>
2024-07-25 00:10:39 -0500

    context_manager.py: Update file for using the database class services.py: update file for using the city_id instead of city_name

--- database/context_manager.py
+++ database/context_manager.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
-from database.base import Base
+from databases import Database
 
 DATABASE_URL = "sqlite:///./test.db"
-
+database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
--- services/services.py
+++ services/services.py
@@ -1,37 +1,37 @@
 import httpx
 import asyncio
 import json
-from sqlalchemy.exc import IntegrityError
 from datetime import datetime
 from typing import Dict, Any
 from models.models import WeatherData
 from database.context_manager import SessionLocal
+from utilities.constants import cities_id_list
 import os
 
 OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
 # make a list of 20 cities
-CITIES = ["London", "Paris", "New York", "Tokyo", "Beijing", "Moscow", "Berlin", "Madrid", "Rome", "Athens", "Cairo", "Nairobi", "Cape Town", "Sydney", "New Delhi", "Bangkok", "Singapore", "Jakarta", "Manila", "Seoul"]
+CITIES = cities_id_list
 
 
-async def fetch_weather_data(city_name: str) -> Dict[str, Any]:
+async def fetch_weather_data(city_id: int) -> Dict[str, Any]:
     async with httpx.AsyncClient() as client:
         response = await client.get(
             f"http://api.openweathermap.org/data/2.5/weather",
-            params={"q": city_name, "appid": OPEN_WEATHER_API_KEY, "units": "metric"}
+            params={"id": city_id, "appid": OPEN_WEATHER_API_KEY, "units": "metric"}
         )
         response.raise_for_status()
         weather_data = response.json()
         return {
-            "city_name": city_name,
+            "city_id": city_id,
             "temperature": weather_data["main"]["temp"],
             "humidity": weather_data["main"]["humidity"]
         }
 
 
-async def fetch_and_store_weather_data(user_id: str):
+async def fetch_and_store_weather_data(user_id: str, cities_list: list = CITIES):
     db = SessionLocal()
-    for city_name in CITIES:
-        weather_info = await fetch_weather_data(city_name)
+    for city_id in cities_list:
+        weather_info = await fetch_weather_data(city_id)
         existing_record = db.query(WeatherData).filter(WeatherData.user_id == user_id).first()
         if existing_record:
             # Load the existing data into a Python list
@@ -50,5 +50,4 @@ async def fetch_and_store_weather_data(user_id: str):
             db.add(weather_data)
         db.commit()  # Commit after each operation
         db.refresh(existing_record or weather_data)
-        await asyncio.sleep(1)
     db.close()
